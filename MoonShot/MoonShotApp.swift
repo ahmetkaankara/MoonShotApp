c7435f6866e6f25f3375904e790d2a850c906692
ahmetkaankara <ahmetkaankara1907@gmail.com>
2022-05-14 15:22:04 +0300

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Ahmet Kaan Kara on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
